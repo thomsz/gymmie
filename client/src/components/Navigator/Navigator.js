56ddbdee593ff0bea726d2eeb3f4722cc9ac9e1f
Tom Segev <hi@tomsegev.com>
2020-11-13 16:49:13 +0200

    Add Category filter mock element

--- client/src/components/Navigator/Navigator.js
+++ client/src/components/Navigator/Navigator.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Select } from 'antd';
+import { Select, Space } from 'antd';
 
 const { Option } = Select;
 
 const Navigator = () => {
-	const startDateChangeHandler = (month) => {
+	const startDateFilterChangeHandler = (month) => {
 		console.log('Filter by a new date', month);
 	};
 
+	const categoryFilterChangeHandler = (category) => {
+		console.log('Filter by a category', category);
+	};
+
+	const children = [];
+	for (let i = 10; i < 36; i++) {
+		children.push(
+			<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>
+		);
+	}
+
 	return (
-		<div>
+		<Space>
 			<Select
 				defaultValue="11"
 				style={{ width: 120 }}
-				onChange={startDateChangeHandler}
+				onChange={startDateFilterChangeHandler}
 			>
 				<Option value="11">November</Option>
 				<Option value="12">December</Option>
@@ -23,7 +34,18 @@ const Navigator = () => {
 				<Option value="4">April</Option>
 				<Option value="5">May</Option>
 			</Select>
-		</div>
+			<Select
+				mode="multiple"
+				allowClear
+				maxTagCount={2}
+				style={{ width: 240 }}
+				placeholder="Filter by category"
+				defaultValue={['b11', 'p25']}
+				onChange={categoryFilterChangeHandler}
+			>
+				{children}
+			</Select>
+		</Space>
 	);
 };
 
