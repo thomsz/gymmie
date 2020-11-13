e2b67366c2da197190d9c9b033932f232ae46c40
Tom Segev <hi@tomsegev.com>
2020-11-13 16:40:53 +0200

    Add Start Date mock element

--- client/src/components/Navigator/Navigator.js
+++ client/src/components/Navigator/Navigator.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Select } from 'antd';
+
+const { Option } = Select;
 
 const Navigator = () => {
-	return <h2>Navigator</h2>;
+	const startDateChangeHandler = (month) => {
+		console.log('Filter by a new date', month);
+	};
+
+	return (
+		<div>
+			<Select
+				defaultValue="11"
+				style={{ width: 120 }}
+				onChange={startDateChangeHandler}
+			>
+				<Option value="11">November</Option>
+				<Option value="12">December</Option>
+				<Option value="1">January</Option>
+				<Option value="2">February</Option>
+				<Option value="3">March</Option>
+				<Option value="4">April</Option>
+				<Option value="5">May</Option>
+			</Select>
+		</div>
+	);
 };
 
 export default Navigator;
