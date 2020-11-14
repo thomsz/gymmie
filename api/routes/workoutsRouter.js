ee66e1795cc86a19a2f70f2656a98e4016aa7b54
Tom Segev <hi@tomsegev.com>
2020-11-14 12:07:41 +0200

    Set up structure for the API endpoints + TODO comments

--- api/routes/workoutsRouter.js
+++ api/routes/workoutsRouter.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+	// TODO: Get all workouts (paginated)
 	res.send('Got workouts');
+});
+
+router.get('/:id', (req, res) => {
+	// TODO: Get workout by id
 });
 
 module.exports = router;
