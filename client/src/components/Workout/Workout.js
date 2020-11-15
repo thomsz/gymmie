6f5c4453e76b95d5e27299017c832a60b778381d
Tom Segev <hi@tomsegev.com>
2020-11-15 15:43:41 +0200

    Format start date

--- client/src/components/Workout/Workout.js
+++ client/src/components/Workout/Workout.js
@@ -9,10 +9,13 @@ const Workout = (props) => {
 
 	const { name, description, startDate, category, image } = data;
 
+	let date = new Date(startDate);
+	date = date.toLocaleDateString('en-US', { month: 'long' });
+
 	return (
 		<PageHeader
 			title={name}
-			subTitle={startDate}
+			subTitle={`Program starts in ${date}`}
 			tags={<Tag>{category}</Tag>}
 			onBack={() => window.history.back()}
 		>
